8cd34532ffd5619733f6b9697a976c2879417c42
juanan <jaehdesign@gmailcom>
2025-02-28 13:40:03 +0100

    actualización prisma 280225

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+console.log('Hello, world!');
+const prisma = new PrismaClient();
+export const getCountries = async () => {
+    const countries = await prisma.country.findMany();
+    console.log(countries);
+};
+export const getCountriesByContinents = async (continent) => {
+    const selectOptions = {
+        code: true,
+        name: true,
+        capital: true,
+    };
+    const countries = await prisma.country.findMany({
+        where: {
+            continent: continent,
+        },
+        select: selectOptions,
+    });
+    console.log(countries);
+};
+// export const getCitiesByCountryCode = async(countryCode: string) => {
+//     const cities = await prisma.city.findMany({
+//         where: {
+//             country: countries,
+//         },
+//     });
+//     console.log(cities)
+// }
+// await getCountries();
+// await getCountriesByContinents('Europe');
+// getCitiesByCountryCode('ESP')
+// getCitiesFromContinentWithCountryName('Europe')
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log('Hello World')
+import { $Enums, PrismaClient, Prisma } from '@prisma/client';
+
+console.log('Hello, world!');
+const prisma = new PrismaClient();
+
+export const getCountries = async () => {
+    const countries = await prisma.country.findMany();
+    console.log(countries);
+};
+
+export const getCountriesByContinents = async (
+    continent: $Enums.CountryContinent,
+) => {
+    const selectOptions: Prisma.CountrySelect = {
+        code: true,
+        name: true,
+        capital: true,
+    };
+
+    const countries = await prisma.country.findMany({
+        where: {
+            continent: continent,
+        },
+        select: selectOptions,
+    });
+    console.log(countries);
+};
+
+// export const getCitiesByCountryCode = async(countryCode: string) => {
+
+//     const cities = await prisma.city.findMany({
+//         where: {
+//             country: countries,
+//         },
+//     });
+//     console.log(cities)
+// }
+
+// await getCountries();
+// await getCountriesByContinents('Europe');
+
+// getCitiesByCountryCode('ESP')
+// getCitiesFromContinentWithCountryName('Europe')
